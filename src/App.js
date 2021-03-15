3c131eb79e75e4c580cd3e86e0724fa08352f947
dwainejade <dwainejade@gmail.com>
2021-03-15 16:37:23 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Components/Nav.js'
+import Hero from './Components/Hero.js'
+
+export default function App() {
+    return (
+        <div>
+            <Nav></Nav>
+            <Hero></Hero>
+        </div>
+    )
+}
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='navbar'>
+            <div className='logo'>Dwaine Matthew</div>
+            <div>Contact</div>
+            <div>Projects</div>
+            <div>Resume</div>
+        </div>
+    )
+}
+
+export default Nav
